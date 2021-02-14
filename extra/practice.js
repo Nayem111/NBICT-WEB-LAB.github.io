fe365992ebd69aa0997fb4a8533f1c2a1ef301c2
Md Nazmus Sakib Nayem <68137240+Nayem111@users.noreply.github.com>
2021-02-14 17:39:07 +0600

    js basic class

---
+++ extra/practice.js
@@ -0,0 +1,17 @@
+let js = 'awesome';
+console.log(js);
+console.log(7-11+2+4-10);
+
+let firstName='nazmus';
+let LastName='sakib nayem';
+console.log(firstName+' '+LastName + ' is an it insturctor and enterpreneur' + firstName+' founded nbict lab ');
+
+let vName= 11;
+let PI=3.14;
+console.log(PI);
+
+let x=10;
+console.log(typeof(x));
+console.log(typeof(firstName));
+console.log(typeof(null));
+
