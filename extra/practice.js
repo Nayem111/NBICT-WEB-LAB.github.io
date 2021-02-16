a6db06cd761c33dc16cd11649dec9749ea9a36b5
Md Nazmus Sakib Nayem <68137240+Nayem111@users.noreply.github.com>
2021-02-16 18:08:05 +0600

    feb.16.1

--- extra/practice.js
+++ extra/practice.js
@@ -1,3 +1,4 @@
+/*
 let js = 'awesome';
 console.log(js);
 console.log(7-11+2+4-10);
@@ -14,6 +15,85 @@ let x=10;
 console.log(typeof(x));
 console.log(typeof(firstName));
 console.log(typeof(null));
+*/
+
+/*let age = 30;
+age = 31;
+console.log(age);
+const birthYear = 1990;
+console.log(birthYear);
+let myName;
+console.log(myName);
+/*const yr;
+console.log(yr);*/
+/* var should not use
+var sub="stat";
+console.log(sub);*/
+
+/*myName='Nazmus Sakib';
+console.log(myName);*/
+
+//mathmatical operator//
+/*const now=2021;
+const nayem= now-1998;
+
+const davidAge=now-1990;
+
+console.log(nayem,davidAge);
+
+console.log(nayem*2,nayem/10, 2**3);
+
+const firstName='nbict';
+const LastName='lab';
+
+console.log(firstName + ' ' + LastName);
+
+// assingment operator//
+let x = 7+11;
+x+=3; //x=x+3//
+x-=4; //x=x-4//
+x++; //x=x+1//
+x--; //x=x-1//
+x--; //x=x-1//
+console.log(x);
+
+//comparison operator//
+console.log(nayem>davidAge);
+console.log(davidAge>=18);
+*/
+
+//exam//
+const country='bangladesh';
+const continent='asia';
+let population=160;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+const isIsland=true;
+const language='bangla';
+console.log(isIsland);
+console.log(language);
+
+console.log(typeof(isIsland));
+console.log(typeof(population));
+console.log(typeof(country));
+console.log(typeof(language));
+
+console.log(language);
+
+
+console.log(population/2);
+console.log(++population);
+
+let finland=6;
+console.log(population>finland);
+
+let averagePopulation=33;
+console.log(population<averagePopulation);
+
+let description= country+ ' '+ 'is' + ' ' + 'in' + continent + ',' + 'it' + ' ' + population +' '+ 'people' + ' '+ 'speak' + ' '+ language;
+console.log(description);
 
 
 
